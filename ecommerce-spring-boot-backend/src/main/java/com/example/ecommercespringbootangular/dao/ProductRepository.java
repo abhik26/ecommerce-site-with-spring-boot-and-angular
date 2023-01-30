ee244f5f0c6d40f78b8e07f84affc0643b521a9e
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2023-01-30 22:26:58 +0530

    Add JPA repository methods to find products by product category id as well as by title search keyword

--- ecommerce-spring-boot-backend/src/main/java/com/example/ecommercespringbootangular/dao/ProductRepository.java
+++ ecommerce-spring-boot-backend/src/main/java/com/example/ecommercespringbootangular/dao/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.example.ecommercespringbootangular.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.ecommercespringbootangular.entity.Product;
@@ -8,4 +11,6 @@ import com.example.ecommercespringbootangular.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
+	Page<Product> findByProductCategoryId(@Param("id") Long id, Pageable pageable);
+	Page<Product> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
 }
