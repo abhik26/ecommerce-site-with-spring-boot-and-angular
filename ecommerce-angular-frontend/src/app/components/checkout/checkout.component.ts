e516ad4b171d5588821fa554aec7f89a3e232b0a
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2023-08-07 20:07:41 +0530

    Add field validation in UI for checkout component.

--- ecommerce-angular-frontend/src/app/components/checkout/checkout.component.ts
+++ ecommerce-angular-frontend/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { CheckoutFormService } from 'src/app/services/checkout-form.service';
@@ -27,34 +27,33 @@ export class CheckoutComponent implements OnInit {
     ngOnInit(): void {
         this.checkoutFormGroup = this.formBuilder.group({
             customer: this.formBuilder.group({
-                firstName: [''],
-                lastName: [''],
-                email: ['']
+                firstName: new FormControl('', [Validators.required, Validators.minLength(2), CheckoutComponent.notOnlyWhiteSpace]),
+                lastName: new FormControl('', [Validators.required, Validators.minLength(2), CheckoutComponent.notOnlyWhiteSpace]),
+                email: new FormControl('', [Validators.required, Validators.email])
             }),
             shippingAddress: this.formBuilder.group({
-                street: [''],
-                city: [''],
-                state: [null],
-                country: [null],
-                zipCode: ['']
+                street: new FormControl('', [Validators.required, Validators.minLength(2), CheckoutComponent.notOnlyWhiteSpace]),
+                city:new FormControl('', [Validators.required, Validators.minLength(2), CheckoutComponent.notOnlyWhiteSpace]),
+                state: new FormControl('', [Validators.required]),
+                country: new FormControl('', [Validators.required]),
+                zipCode: new FormControl('', [Validators.required, Validators.minLength(6), CheckoutComponent.notOnlyWhiteSpace])
             }),
             billingAddress: this.formBuilder.group({
-                street: [''],
-                city: [''],
-                state: [null],
-                country: [null],
-                zipCode: ['']
+                street: new FormControl('', [Validators.required, Validators.minLength(2), CheckoutComponent.notOnlyWhiteSpace]),
+                city:new FormControl('', [Validators.required, Validators.minLength(2), CheckoutComponent.notOnlyWhiteSpace]),
+                state: new FormControl('', [Validators.required]),
+                country: new FormControl('', [Validators.required]),
+                zipCode: new FormControl('', [Validators.required, Validators.minLength(6), CheckoutComponent.notOnlyWhiteSpace])
             }),
             creditCard: this.formBuilder.group({
-                cartType: [''],
-                nameOnCard: [''],
-                cardNumber: [''],
-                securityCode: [''],
-                expiryMonth: [''],
-                expiryYear: ['']
+                cardType: new FormControl('', [Validators.required]),
+                nameOnCard: new FormControl('', [Validators.required, Validators.minLength(2), CheckoutComponent.notOnlyWhiteSpace]),
+                cardNumber: new FormControl('', [Validators.required, Validators.pattern('[0-9]{16}')]),
+                securityCode: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}')]),
+                expiryMonth: new FormControl('', [Validators.required]),
+                expiryYear: new FormControl('', [Validators.required])
             })
         });
-
         
         this.checkoutFromService.getCreditCardMonths(new Date().getMonth() + 1).subscribe(
             data => this.creditCardMonths = data
@@ -67,6 +66,30 @@ export class CheckoutComponent implements OnInit {
             data => this.countries = data
         );
     }
+
+    get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+    get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+    get email() { return this.checkoutFormGroup.get('customer.email'); }
+    
+    get shippingAddressStreet() { return this.checkoutFormGroup.get('shippingAddress.street'); }
+    get shippingAddressCity() { return this.checkoutFormGroup.get('shippingAddress.city'); }
+    get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state'); }
+    get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
+    get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
+
+    get billingAddressStreet() { return this.checkoutFormGroup.get('billingAddress.street'); }
+    get billingAddressCity() { return this.checkoutFormGroup.get('billingAddress.city'); }
+    get billingAddressState() { return this.checkoutFormGroup.get('billingAddress.state'); }
+    get billingAddressCountry() { return this.checkoutFormGroup.get('billingAddress.country'); }
+    get billingAddressZipCode() { return this.checkoutFormGroup.get('billingAddress.zipCode'); }
+
+    get creditCardCardType() { return this.checkoutFormGroup.get('creditCard.cardType'); }
+    get creditCardNameOnCard() { return this.checkoutFormGroup.get('creditCard.nameOnCard'); }
+    get creditCardCardNumber() { return this.checkoutFormGroup.get('creditCard.cardNumber'); }
+    get creditCardSecurityCode() { return this.checkoutFormGroup.get('creditCard.securityCode'); }
+    get creditCardExpiryMonth() { return this.checkoutFormGroup.get('creditCard.expiryMonth'); }
+    get creditCardExpiryYear() { return this.checkoutFormGroup.get('creditCard.expiryYear'); }
+
 
     checkBillingAddressWithShippingAddress(event: any) {
         if (event.target.checked) {
@@ -84,7 +107,11 @@ export class CheckoutComponent implements OnInit {
     }
 
     purchaseProducts() {
-        console.log(this.checkoutFormGroup.value);
+        if (this.checkoutFormGroup.invalid) {
+            this.checkoutFormGroup.markAllAsTouched();
+        } else {
+            console.log(this.checkoutFormGroup.value);
+        }
     }
 
     handleCreditCardExpiryYearChange() {
@@ -113,6 +140,14 @@ export class CheckoutComponent implements OnInit {
                 }
             }
         );
+    }
+
+    static notOnlyWhiteSpace(formControl: FormControl): ValidationErrors {
+        if (formControl.value && formControl.value.trim().length === 0) {
+            return { 'onlyWhiteSpace': true };
+        } else {
+            return null;
+        }
     }
 
 }
