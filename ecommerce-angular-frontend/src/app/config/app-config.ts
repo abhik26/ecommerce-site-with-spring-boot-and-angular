12fc5cf675f078f21dbb66d1432c8bf1e06c437a
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2023-08-22 20:20:52 +0530

    Add self signed open ssl key and certificate to run the Angular app on http(s).

--- ecommerce-angular-frontend/src/app/config/app-config.ts
+++ ecommerce-angular-frontend/src/app/config/app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oaasdwsjzYDqQK2z5d7',
         issuer: 'https://dev-11798857.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
